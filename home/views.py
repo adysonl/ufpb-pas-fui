34d0e8052f3f548e3667c8832248ada8a45057d4
ewertonP <ewerton.paulo@dce.ufpb.br>
2018-09-18 20:17:02 -0300

    bug fixes, cad-user and andress model

--- home/views.py
+++ home/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-@login_required
 def index(request):
     context_dict = {'nome_do_prato': "Pizza de Pepperoni"}
     return render(request, 'home/index.html', context=context_dict)
