2458b32a7917867d9a7312c19a7369c95886a672
unknown <abias.santana@dce.ufpb.br>
2018-09-27 09:49:39 -0300

    redirect for perfil after edite

--- user/views.py
+++ user/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from .forms import usuario_form
 from django.contrib.auth.models import User
 from user.models import User_animal, Address
 from django.contrib import messages
+from perfil.views import perfil
 from perfil import urls
 
 # Create your views here.
@@ -38,8 +40,9 @@ def cadastro_usuario(request):
                     Address.objects.filter(id = old_usuario.address.id).update(id = old_usuario.address.id,street=street, number=number, city=city, neighborhood=neighborhood, df=df)
                     User_animal.objects.filter(user=user_test.id).update(type='Animal', user=user_test.id, address=old_usuario.address.id, rg=rg, cpf=cpf,telephone=telephone)
                     User.objects.filter(id = user_test.id).update(username=username, first_name=name, last_name=last_name,email=email, password=make_password(password))
-
-                    return redirect('perfil')
+                    user = User.objects.get(username=username)
+                    login(request, user)
+                    return redirect('/user/perfil')
                 if user_test:
                     messages.error(request, 'Usuário existe!')
             except User.DoesNotExist:
