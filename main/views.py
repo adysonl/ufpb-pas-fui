b39e32e6fffc51c32f286f55d5837b668c51dd02
ewertonP <ewerton.paulo@dce.ufpb.br>
2018-10-13 18:42:43 -0300

    Event list home

--- main/views.py
+++ main/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate, logout
 from .forms import LoginForm
+from event.models import Event
 
 # Create your views here.
 
 def index(request):
-    context_dict = {'nome_do_prato': "Pizza de Pepperoni"}
+    events = Event
+    events = Event.objects.all()
+    context_dict = {'events': events}
     return render(request, 'home/index.html', context=context_dict)
 
 def search(request):
